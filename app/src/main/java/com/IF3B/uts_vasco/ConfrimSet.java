8f0a489fafc7967558795a036a140e155fbda64d
Vasco <VascoKing21@mhs.mdp.ac.id>
2022-11-02 15:00:39 +0700

    UTS_Vascodegamma_1923250025

---
+++ app/src/main/java/com/IF3B/uts_vasco/ConfrimSet.java
@@ -0,0 +1,35 @@
+package com.IF3B.uts_vasco;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class ConfrimSet extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_confrim_sent);
+
+        String nama, nomor, jalur;
+
+        TextView tvNama, tvNomor, tvOpt;
+
+        tvNama = findViewById(R.id.tvNama);
+        tvNomor = findViewById(R.id.tvNomor);
+        tvOpt = findViewById(R.id.tvOpt);
+
+        Intent terima = getIntent();
+
+        nama = terima.getStringExtra("VarNama");
+        nomor = terima.getStringExtra("VarNomor");
+        jalur = terima.getStringExtra("VarJalur");
+
+        tvNama.setText(nama);
+        tvNomor.setText(nomor);
+        tvOpt.setText(jalur);
+
+
+
+    }
+}
